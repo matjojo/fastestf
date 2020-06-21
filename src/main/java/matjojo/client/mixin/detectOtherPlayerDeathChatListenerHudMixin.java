a71060d6c7cdbc455845efb65cbb14eb3068301a
matjojo <mat2904jojo@gmail.com>
2020-06-21 20:51:22 +0200

    Implement feature, remove reliance on fapi and remove useless entrypoint

---
+++ src/main/java/matjojo/client/mixin/detectOtherPlayerDeathChatListenerHudMixin.java
@@ -0,0 +1,40 @@
+package matjojo.client.mixin;
+
+import matjojo.client.util;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.ChatListenerHud;
+import net.minecraft.network.MessageType;
+import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
+
+@Mixin(ChatListenerHud.class)
+public abstract class detectOtherPlayerDeathChatListenerHudMixin {
+
+	@Environment(EnvType.CLIENT)
+	@Inject(method = "onChatMessage(Lnet/minecraft/network/MessageType;Lnet/minecraft/text/Text;Ljava/util/UUID;)V",
+	at = @At(value = "HEAD"))
+	private void onChatMessageMixin(MessageType type, Text text, UUID id, CallbackInfo ci) {
+		if (!util.isUsableDeathMessage(type, text)) {
+			return;
+		}
+		// if it cannot be casted to Translatable it would not be a usable death message
+		if (!util.deathMessageIsAboutOtherPlayer((TranslatableText) text)) {
+			return;
+		}
+		// This is only the case when no world is open
+		// but a message will never be added to the chat of a world outside a world
+		assert MinecraftClient.getInstance().player != null;
+		MinecraftClient.getInstance().player.networkHandler.sendPacket(
+				new ChatMessageC2SPacket("F")
+		);
+	}
+}
---
+++ src/main/java/matjojo/client/util.java
@@ -0,0 +1,69 @@
+package matjojo.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+
+public class util {
+
+	/**
+	 * Check if the passed message and type are part of a death message usable for died-player name checking.
+	 * @param type The MessageType sent with the Text
+	 * @param text The Text that is checked to be a usable death message
+	 * @return false if the message is either of the wrong type or does not have the correct arguments
+	 */
+	public static boolean isUsableDeathMessage(MessageType type, Text text) {
+		if (type != MessageType.SYSTEM) {
+			return false;
+		}
+		if (!(text instanceof TranslatableText)) {
+			return false;
+		}
+		TranslatableText translatableText = (TranslatableText) text;
+		if (!translatableText.getKey().startsWith("death.")) {
+			return false;
+		}
+
+		// there is one death message without arguments: "death.attack.badRespawnPoint.link"
+		// for now we can just ignore that one, since we can't really check if that was not us.
+		if (translatableText.getArgs().length == 0) {
+			return false;
+		}
+		// this is always true, even in the message
+		// "death.attack.message_too_long" which takes a list of arguments,
+		// but that list is always a list of 1 Literaltext,
+		// in ServerPlayerEntity:480.
+		if (!(translatableText.getArgs()[0] instanceof LiteralText)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Checks if the passed death message Text is about another player dying.
+	 * @param deathMessage The text that contains a usable (see isUsableDeathMessage) death message
+	 * @return true if the player that died was not this client's player.
+	 */
+	@Environment(EnvType.CLIENT)
+	public static boolean deathMessageIsAboutOtherPlayer(TranslatableText deathMessage) {
+		ClientPlayerEntity clientPlayerEntity = MinecraftClient.getInstance().player;
+		if (clientPlayerEntity == null) {
+			return false;
+		}
+		if (!(clientPlayerEntity.getName() instanceof LiteralText)) {
+			return false;
+		}
+		LiteralText diedPlayerName = (LiteralText) deathMessage.getArgs()[0];
+		LiteralText currentPlayerName = (LiteralText) clientPlayerEntity.getName();
+		if (diedPlayerName.getRawString().equals(currentPlayerName.getRawString())) {
+			return false;
+		}
+		return true;
+	}
+}
